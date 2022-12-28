68b5977b3c843fbb959015a5e7763a3c834ac392
xzy202110120311 <121210947+xzy202110120311@users.noreply.github.com>
2022-12-28 20:54:12 +0800

    Add files via upload

---
+++ 函数的定义.cpp
@@ -0,0 +1,32 @@
+void login()  //登录的函数
+
+void revisePassage() //修改密码的函数
+
+void choice()  //在大厅选择的函数
+
+void reviseUser() //用户修改信息的函数
+
+void reviseAdministrator() //管理员修改信息的函数
+
+void display() //显示个人信息的函数
+
+void inquire() //查询书籍的函数
+
+void score() //信用分功能的函数
+
+void number() //借书数量限制的函数
+
+void reservation() //预约功能的函数
+
+void borrow() //借书功能的函数
+
+void Return() //还书功能的函数
+
+void ID() //图书编号功能的函数
+
+void account() //收集用户和管理员账号的函数
+
+void manage() //管理员管理用户的函数
+
+void storage() //管理员对书籍信息的存储和管理
+
